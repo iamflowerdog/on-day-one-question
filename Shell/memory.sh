b72dc33e7a8f5cf45a9045e20591f1f364dd9c3a
Mr.YanG <yangyonghai9125@hotmail.com>
2022-03-22 15:31:58 +0800

    1. 持续输出到多个文件

--- Shell/memory.sh
+++ Shell/memory.sh
@@ -1,8 +1,17 @@
 # file /tmp/1.sh
 #!/bin/bash
-logfile="./zeekmem.txt"
-echo "   PID USER      PR  NI    VIRT    RES    SHR S  %CPU  %MEM     TIME+ COMMAND"> "$logfile"
+zeek="./zeek.txt"
+java="./java.txt"
+omnibeat="./omni-beat.txt"
+thunderbot="./thunder-bot.txt"
+echo "   PID USER      PR  NI    VIRT    RES    SHR S  %CPU  %MEM     TIME+ COMMAND"> "$zeek"
+echo "   PID USER      PR  NI    VIRT    RES    SHR S  %CPU  %MEM     TIME+ COMMAND"> "$java"
+echo "   PID USER      PR  NI    VIRT    RES    SHR S  %CPU  %MEM     TIME+ COMMAND"> "$omnibeat"
+echo "   PID USER      PR  NI    VIRT    RES    SHR S  %CPU  %MEM     TIME+ COMMAND"> "$thunderbot"
 for i in {1..10000000};do
-    top -b -n 1 | grep -E 'zeek|java|thunder-bot|omni-beat|omni-magnet|omni-leach|omni-decode|omni-sysinfo' >> "$logfile"
+    top -b -n 1 | grep -E 'zeek' >> "$zeek"
+    top -b -n 1 | grep -E 'java' >> "$java"
+    top -b -n 1 | grep -E 'thunder-bot' >> "$thunderbot"
+    top -b -n 1 | grep -E 'omni-beat' >> "$omnibeat"
     sleep 60
 done
