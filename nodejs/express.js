5f2b99a176ba9b914d5e0e6e01ed2156a09601ae
yang <yangyonghai9125@hotmail.com>
2023-12-04 08:51:29 +0800

    test

---
+++ nodejs/express.js
@@ -0,0 +1,26 @@
+
+
+
+switchFun(2)
+
+
+function switchFun(type) {
+    switch (type > 0) {
+        case true:
+            console.log('true')
+            break;
+
+        default:
+            break;
+    }
+
+
+    switch (type > 1) {
+        case true:
+            console.log('1')
+            break;
+
+        default:
+            break;
+    }
+}
