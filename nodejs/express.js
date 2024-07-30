26b22c1e509f984a16065bfb5d7ed67f4ee111ca
yang <yangyonghai9125@hotmail.com>
2024-07-30 13:34:34 +0800

    update resume

--- nodejs/express.js
+++ nodejs/express.js
@@ -1,26 +1,13 @@
-
-
-
-switchFun(2)
-
-
-function switchFun(type) {
-    switch (type > 0) {
-        case true:
-            console.log('true')
-            break;
-
-        default:
-            break;
-    }
-
-
-    switch (type > 1) {
-        case true:
-            console.log('1')
-            break;
-
-        default:
-            break;
-    }
-}
+function* generatorExample() {
+    yield 1;
+    yield 2;
+    yield 3;
+  }
+  
+  const gen = generatorExample();
+  
+  console.log(gen.next().value); // 输出: 1
+  console.log(gen.next().value); // 输出: 2
+  console.log(gen.next().value); // 输出: 3
+  console.log(gen.next().done);  // 输出: true
+  
