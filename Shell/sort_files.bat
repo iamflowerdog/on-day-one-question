84fcc23b6bb01ad8c91a8af65474df7968caa807
yang.yang <yangyonghai9125@hotmail.com>
2024-09-29 09:34:57 +0800

    test

--- Shell/sort_files.bat
+++ Shell/sort_files.bat
@@ -1,60 +1,35 @@
 @echo off
 setlocal enabledelayedexpansion
 
-:: 参数设置
-set "source_dir=%~dp0"  :: 当前目录
-set "files_per_folder=%1"  :: 每个子文件夹的文件数量，可以通过参数传递
+:: 设置每个子文件夹中的文件数量（默认为5，可通过参数更改）
+set files_per_folder=5
+if not "%~1"=="" set files_per_folder=%~1
 
-if "%files_per_folder%"=="" (
-    set files_per_folder=5  :: 如果没有提供参数，默认每个文件夹5个文件
+:: 获取当前目录中的文件数量
+set file_count=0
+for %%F in (*) do set /a file_count+=1
+
+:: 计算需要创建的文件夹数量
+set /a folder_count=(file_count+files_per_folder-1)/files_per_folder
+
+:: 创建子文件夹
+for /l %%i in (1,1,%folder_count%) do (
+    set /a start=((%%i-1)*files_per_folder)+1
+    set /a end=%%i*files_per_folder
+    if !end! gtr %file_count% set end=%file_count%
+    md "!start!-!end!"
 )
 
-:: 检查是否有文件
-cd "%source_dir%"
-set "file_count=0"
-for %%F in (*.*) do (
-    set /a file_count+=1
+:: 移动文件到对应的子文件夹
+set current_folder=1
+set file_index=0
+for %%F in (*) do (
+    set /a file_index+=1
+    set /a current_folder=((file_index-1)/files_per_folder)+1
+    set /a start=((current_folder-1)*files_per_folder)+1
+    set /a end=current_folder*files_per_folder
+    if !end! gtr %file_count% set end=%file_count%
+    move "%%F" "!start!-!end!\"
 )
 
-echo 当前目录下文件总数: %file_count%
-
-:: 如果没有文件，提示错误并退出
-if %file_count%==0 (
-    echo 当前目录下没有任何文件，脚本退出
-    pause
-    exit /b
-)
-
-:: 开始按数量创建子文件夹并移动文件
-set /a folder_count=1
-set /a start_index=1
-set /a end_index=%files_per_folder%
-
-for /l %%G in (1,%files_per_folder%,%file_count%) do (
-    :: 生成文件夹名
-    if %end_index% gtr %file_count% (
-        set end_index=%file_count%
-    )
-    
-    set "folder_name=%start_index%-%end_index%"
-    
-    :: 创建子文件夹
-    mkdir "%folder_name%"
-    echo 创建文件夹 %folder_name%
-    
-    :: 移动文件到子文件夹
-    set /a count=0
-    for %%F in (*.*) do (
-        if !count! geq %start_index% if !count! lss %end_index% (
-            echo 移动文件 %%F 到 %folder_name%
-            move "%%F" "%folder_name%"
-        )
-        set /a count+=1
-    )
-    
-    set /a start_index+=%files_per_folder%
-    set /a end_index+=%files_per_folder%
-)
-
-echo 文件移动完成
-pause
+echo 文件整理完成。
